a5adf22a83dd718f4c3b60ffcd602a5bd8a0c57f
Yegor Bugayenko <yegor@tpc2.com>
2016-03-14 15:36:08 -0700

    forms better

--- js/all.js
+++ js/all.js
@@ -1,41 +1,28 @@
 /*globals $:false, window:false, document:false, alert:false */
-function send_email(text, success, error) {
-  'use strict';
-  $.ajax(
-    {
-      type: 'POST',
-      url: 'http://formspree.io/yegor@teamed.io',
-      data: {
-        'message': 'Hi,\n\n' + text
-          + '\n\nThanks'
-          + '\n\n--\nsent through at.teamed.io',
-        '_subject': 'new form submitted'
-      },
-      success: success,
-      error: error
-    }
-  );
-}
 var email = function(form) {
   'use strict';
   var $form = $(form),
     $button = $form.find('button'),
     before = $button.text(),
-    text = '';
+    data = {};
   $button.prop('disabled', true).text('processing...');
   $form.find('input,textarea').each(
     function() {
       var $i = $(this);
-      text += $i.prop('name') + ': ' + $i.val() + '\n\n';
+      data[$i.prop('name')] = $i.val();
     }
   );
-  send_email(
-    text,
-    function () {
-      $button.text('thanks!');
-    },
-    function () {
-      $button.prop('disabled', false).text(before);
+  $.ajax(
+    {
+      type: 'POST',
+      url: 'http://formspree.io/yegor@teamed.io',
+      data: data,
+      success: function () {
+        $button.text('thanks!');
+      },
+      error: function () {
+        $button.prop('disabled', false).text(before);
+      }
     }
   );
 };
